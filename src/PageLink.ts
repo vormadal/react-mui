38808d966d8d4ca82a88e762426c4315f74970eb
Rúni Vørmadal <runivormadal@gmail.com>
2023-08-16 16:38:56 +0200

    added documentation for all components and hooks

--- src/PageLink.ts
+++ src/PageLink.ts
@@ -1,9 +1,28 @@
 export interface PageLink {
+  /**
+   * The name or label shown in menu or drawer of the navigation bar
+   */
   name: string
+  /**
+   * The link the user is navigated to when clicking the item in menu or drawer.
+   * **Note:** Currently only relative links are supported
+   */
   path: string
+
+  /**
+   * If **true** this link will be hidden when the user is not logged in.
+   */
   protected?: boolean
 
-  // only applies to drawer
+  /**
+   * **Not used**
+   * Only applies to drawers allowing to add dividers between links.
+   * The divider will be added below this link, thus the `name` and `path` will still be required
+   */
   divider?: boolean
+
+  /**
+   * When using drawer you can specify an icon which will be shown to the left of the link
+   */
   icon?: React.ReactNode
 }
